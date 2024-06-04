0dcf3c10c066cb40fd1808742b93267e84f7f35c
Adrian <adferrer0611@gmail.com>
2024-06-04 13:32:18 -0400

    Test unitarios de AuthenticationServices hechos

--- Medicare.Tests/Unit/Application/AuthenticationServiceTests.cs
+++ Medicare.Tests/Unit/Application/AuthenticationServiceTests.cs
@@ -12,33 +12,99 @@ namespace Medicare.Tests.Unit.Application
 		private Mock<IUserService> _userServiceMock;
 		private IPasswordManager _passwordManager;
 		private IAuthenticationService _authenticationService;
+		string basePassword = "\"AdrianDev777!\"";
+		string baseUsername = "adrian_dev777";
+		User recoveredUser;
 
         public AuthenticationServiceTests()
         {
             _userServiceMock = new Mock<IUserService>();
 			_passwordManager = new PasswordManager();
 			_authenticationService = new AuthenticationService(_passwordManager,_userServiceMock.Object);
-        }
+			recoveredUser = new User
+			{
+				Username = baseUsername,
+				Password = _passwordManager.HashPassword(basePassword)
+			};
+			_userServiceMock.Setup(userService => userService.UserExists(It.IsAny<string>(), It.IsAny<CancellationToken>()))
+	.ReturnsAsync(recoveredUser);
+		}
 
 		[Fact]
 		public async Task LogIn_When_UserExists_And_HasValidCredentials()
 		{
-			string basePassword = "\"AdrianDev777!\"";
-			User recoveredUser = new User { 				
-				Username = "adrian_dev777",
-				Password = _passwordManager.HashPassword(basePassword)
-			};
-
+			
 			User user = new User
 			{
-				Username = "adrian_dev777",
+				Username = baseUsername,
 				Password = basePassword
 			};
 
-			_userServiceMock.Setup(userService => userService.UserExists(It.IsAny<string>(), It.IsAny<CancellationToken>())).ReturnsAsync(recoveredUser);
 			bool result = await _authenticationService.LogIn(user, new CancellationToken());
 
 			Assert.True(result);
+		}
+
+		[Theory]
+		[InlineData("adrianDev777!")]
+		[InlineData("adrian_dev777")]
+		[InlineData("AdrianDev777")]
+		[InlineData("NotValidPassword")]
+		[InlineData("AdrianDev777! ")]
+		[InlineData("")]
+		public async Task LogIn_When_UserExists_And_HasInvalidPassword(string password)
+		{
+
+			User user = new User
+			{
+				Username = baseUsername,
+				Password = password
+			};
+
+			bool result = await _authenticationService.LogIn(user, new CancellationToken());
+			Assert.False(result);
+		}
+
+
+		[Theory]
+		//[InlineData("adrian")]
+		[InlineData("leonardo_taveras")]
+		[InlineData("juancito")]
+		[InlineData("NotValidUser")]
+		//[InlineData("adrian_dev777 ")]
+		[InlineData("")]
+		public async Task LogIn_When_UserDoesntExist_And_HasValidPassword(string username)
+		{
+			User userNotFound = null;
+			_userServiceMock.Setup(userService => userService.UserExists(It.IsAny<string>(), It.IsAny<CancellationToken>()))
+	.ReturnsAsync(userNotFound);
+			User user = new User
+			{
+				Username = username,
+				Password = basePassword
+			};
+
+			bool result = await _authenticationService.LogIn(user, new CancellationToken());
+			Assert.False(result);
+		}
+
+		[Theory]
+		[InlineData("adrian", "Jamskwk")]
+		[InlineData("leonardo_taveras", "profesor")]
+		[InlineData("juancito", "sport")]
+		[InlineData("NotValidUser", "NotValidPassword")]
+		[InlineData("adrian_dev777 ", "AdrianDev777! ")]
+		[InlineData("", "")]
+		public async Task LogIn_When_InvalidCredentials(string username, string password)
+		{
+			User user = new User
+			{
+				Username = username,
+				Password = password
+			};
+
+			bool result = await _authenticationService.LogIn(user, new CancellationToken());
+			Assert.False(result);
 		}
 	}
 }
